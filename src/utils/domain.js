ded4d1e63c72cc5c860917b4af3f09fa57ff44eb
biaochen <834301747@qq.com>
2019-05-12 16:16:14 +0800

    更正文档

--- src/utils/domain.js
+++ src/utils/domain.js
@@ -1,4 +1,4 @@
-let domain = 'http://biaochenxuying.cn/'; // 正式域名
+let domain = 'https://biaochenxuying.cn/'; // 正式域名
 if (process.env.NODE_ENV === 'development') {
   // 开发环境下，本地地址
   domain = 'http://localhost:3001/';
